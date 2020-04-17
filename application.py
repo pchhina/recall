705f57c05621f3bb34b42109c7b64da971ac2fc0
Paramjot Singh <param.chhina@gmail.com>
2020-04-17 07:11:20 -0500

    barebone registration implemented

--- application.py
+++ application.py
@@ -1,15 +1,33 @@
 from flask import Flask, redirect, render_template, request
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+
 
 # Configure application
 app = Flask(__name__)
+
+# Connect to the database
+conn = sqlite3.connect("recall.db", check_same_thread=False)
+db = conn.cursor()
 
 # Wish List 
 
 # str str -> insert into db
 # registers a new user
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        name = request.form.get("username")
+        pwd = request.form.get("pwd")
+        pwd2 = request.form.get("pwd2")
+        print(name, pwd, pwd2)
+        pwd_hash = generate_password_hash(pwd)
+        ins = "INSERT INTO users (username, hash) VALUES (?, ?)"
+        conn.execute(ins, (name, pwd_hash))
+        conn.commit()
+        return redirect("/")
 
 # str str -> check db
 # login a user
